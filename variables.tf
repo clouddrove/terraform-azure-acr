cfb994ef34b652370ec22aa565756bd825c0a8dd
Deepak Verma <deepak.verma@clouddrove.com>
2024-02-13 18:21:36 +0530

    fmt run

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "repository" {
 
 variable "label_order" {
   type        = list(any)
-  default     = ["name","environment"]
+  default     = ["name", "environment"]
   description = "Label order, e.g. sequence of application name and environment `name`,`environment`,'attribute' [`webserver`,`qa`,`devops`,`public`,] ."
 }
 
