7b594c5a5c7b3882f46cf31762456fe909aa9db7
Ravi Malvia <ravi.malvia@clouddrove.com>
2024-07-23 16:35:32 +0530

    fix: Added data block to get existing dns zone id and updated the subnet version and added diff sub option

--- _example/complete/example.tf
+++ _example/complete/example.tf
@@ -1,6 +1,16 @@
 provider "azurerm" {
   features {}
+  subscription_id            = "01111111111110-11-11-11-11"
+  skip_provider_registration = "true"
 }
+
+provider "azurerm" {
+  features {}
+  alias                      = "peer"
+  subscription_id            = "01111111111110-11-11-11-11"
+  skip_provider_registration = "true"
+}
+
 
 data "azurerm_client_config" "current_client_config" {}
 
@@ -43,7 +53,7 @@ module "vnet" {
 ##-----------------------------------------------------------------------------
 module "subnet" {
   source               = "clouddrove/subnet/azure"
-  version              = "1.1.0"
+  version              = "1.2.0"
   name                 = local.name
   environment          = local.environment
   resource_group_name  = module.resource_group.resource_group_name
@@ -77,17 +87,25 @@ module "log-analytics" {
   log_analytics_workspace_location = module.resource_group.resource_group_location
 }
 
+
+#########Following to be uncommnented only when using DNS Zone from different subscription along with existing DNS zone.
+# data "azurerm_private_dns_zone" "existing_dns_zone" {
+#   name                = "privatelink.azurecr.io" # The name of your DNS Zone
+#   resource_group_name = "dns-rg"                 # The resource group where existing the DNS Zone is located
+# }
+
+
 module "vault" {
   source              = "clouddrove/key-vault/azure"
   version             = "1.1.0"
-  name                = "apptest4rds3474"
+  name                = "apptest4rds3477"
   environment         = local.environment
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
   virtual_network_id  = module.vnet.vnet_id
   subnet_id           = module.subnet.default_subnet_id[0]
 
-  public_network_access_enabled = false
+  public_network_access_enabled = true
 
   network_acls = {
     bypass         = "AzureServices"
@@ -104,29 +122,47 @@ module "vault" {
   log_analytics_workspace_id = module.log-analytics.workspace_id ## when diagnostic_setting_enable = true, need to add log analytics workspace id
 }
 
+
 ##----------------------------------------------------------------------------- 
 ## ACR module call.
 ##-----------------------------------------------------------------------------
 module "container-registry" {
+  providers = {
+    azurerm.dns_sub  = azurerm.peer,
+    azurerm.main_sub = azurerm
+  }
   source              = "../../"
   name                = local.name # Name used for specifying tags and other resources naming.(like private endpoint, vnet-link etc)
   environment         = local.environment
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
   container_registry_config = {
-    name = "cdacr1234" # Name of Container Registry
-    sku  = "Basic"
+    name = "cdacr1232" # Name of Container Registry
+    sku  = "Premium"
   }
   log_analytics_workspace_id = module.log-analytics.workspace_id
+
   ##----------------------------------------------------------------------------- 
   ## To be mentioned for private endpoint, because private endpoint is enabled by default.
   ## To disable private endpoint set 'enable_private_endpoint' variable = false and than no need to specify following variable  
   ##-----------------------------------------------------------------------------
   virtual_network_id = module.vnet.vnet_id
   subnet_id          = module.subnet.default_subnet_id[0]
+  ########Following to be uncommnented only when using DNS Zone from different subscription along with existing DNS zone.
+
+  # diff_sub = true
+  # alias                                         = ""
+  # alias_sub                                     = ""
+
+  #########Following to be uncommmented when using DNS zone from different resource group or different subscription.
+  #existing_private_dns_zone                     = "privatelink.azurecr.io"
+  #existing_private_dns_zone_resource_group_name = "dns-rg"
+  #existing_private_dns_zone_id                  = [data.azurerm_private_dns_zone.existing_dns_zone.id]
+  
   ##if encryption is enabled.
   encryption                  = true
   enable_content_trust        = false
   key_vault_rbac_auth_enabled = true
   key_vault_id                = module.vault.id
 }
+
